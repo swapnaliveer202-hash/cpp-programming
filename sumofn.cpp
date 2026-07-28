3a66ebca6ba637b63d053551c60b95061fcb9978
swapnaliveer202-hash <swapnaliveer202@gmail.com>
2026-07-28 14:45:42 +0530

    Add files via upload

---
+++ sumofn.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+int sum;
+cout<<"enter a number:";
+cin>>n;
+for
+( int i=1;i<=n;i++)
+{
+sum= sum+i;
+}
+cout<<"sum of first"<<n<<"natural numbers="<<sum;
+return 0;
+}
---
+++ swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter first number"<<endl;
+cin>>a;
+cout<<"enter second number"<<endl;
+cin>>b;
+c=a;
+a=b;
+b=c;
+cout<<"the value of a is"<<"and the value of b is"<<b;
+return 0;
+}
